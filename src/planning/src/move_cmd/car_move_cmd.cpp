30e5154db7b27ea046724e8a4182b18f36653653
weiweiweidong <dongweisp@gmail.com>
2026-02-03 23:29:06 +0900

    主车运动的初始化

--- src/planning/src/move_cmd/car_move_cmd.cpp
+++ src/planning/src/move_cmd/car_move_cmd.cpp
@@ -2,14 +2,40 @@
 
 namespace Planning
 {
-    CarMoveCmd::CarMoveCmd() : Node("car_move_cmd_node")    // 主车运动指令
+    CarMoveCmd::CarMoveCmd() : Node("car_move_cmd_node") // 主车运动指令
     {
-        RCLCPP_INFO(this->get_logger() , "car_move_cmd_node created");
+        RCLCPP_INFO(this->get_logger(), "car_move_cmd_node created");
+
+        // 读取配置文件，获取父子坐标
+        move_cmd_config_ = std::make_unique<ConfigReader>();
+        move_cmd_config_->read_move_cmd_config(); // 虽然运动模块本身没有配参数，但是点进去后会发现运动模块本身会读取地图的坐标系。所以还是要写这一句
+
+        // 初始化
+        car_ = std::make_shared<MainCar>();
+        car_param_.pos_x_ = car_->loc_point().pose.position.x;
+        car_param_.pos_y_ = car_->loc_point().pose.position.y;
+        car_param_.theta_ = car_->theta();
+        car_param_.speed_ = car_->speed();
+
+        // 创建坐标广播方
+        broadcaster_ = std::make_shared<TransformBroadcaster>(this);
+
+        // 创建轨迹订阅器，广播运动指令
+        local_trajectory_sub_ = this->create_subscription<LocalTrajectory>(
+            "planning/local_trajectory",
+            10,
+            std::bind(&CarMoveCmd::car_broadcast_tf, this, _1));
     }
-}  // namespace Planning
 
+    // 广播主车坐标变换
+    void CarMoveCmd::car_broadcast_tf(const LocalTrajectory::SharedPtr trajectory)
+    {
+        (void)trajectory;
+    }
+} // namespace Planning
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[])
+{
     rclcpp::init(argc, argv);
     auto node = std::make_shared<Planning::CarMoveCmd>();
     rclcpp::spin(node);
--- src/planning/src/move_cmd/car_move_cmd.h
+++ src/planning/src/move_cmd/car_move_cmd.h
@@ -2,16 +2,44 @@
 #define CAR_MOVE_CMD_H_
 
 #include "rclcpp/rclcpp.hpp"
+#include "base_msgs/msg/local_trajectory.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include <cmath>
 #include "config_reader.h"
 #include "main_car_info.h"
 
 namespace Planning
 {
-    class CarMoveCmd : public rclcpp::Node  // 主车运动指令
+    using namespace std::chrono_literals;
+    using std::placeholders::_1;
+
+    using base_msgs::msg::LocalTrajectory;
+    using geometry_msgs::msg::TransformStamped;
+    using tf2_ros::TransformBroadcaster;
+
+    struct car_param
     {
-        public:
-            CarMoveCmd();
-        private:
+        double pos_x_ = 0.0; // 位置x
+        double pos_y_ = 0.0; // 位置y
+        double theta_ = 0.0; // 航向角
+        double speed_ = 0.0; // 速度
     };
-}  // namespace Planning
-#endif  // CAR_MOVE_CMD_H_
+
+    class CarMoveCmd : public rclcpp::Node // 主车运动指令
+    {
+    public:
+        CarMoveCmd();
+
+    private:
+        void car_broadcast_tf(const LocalTrajectory::SharedPtr trajectory); // 广播主车坐标变换
+
+    private:
+        std::unique_ptr<ConfigReader> move_cmd_config_;                         // 配置
+        std::shared_ptr<TransformBroadcaster> broadcaster_;                     // 广播方
+        rclcpp::Subscription<LocalTrajectory>::SharedPtr local_trajectory_sub_; // 轨迹订阅器
+        std::shared_ptr<VehicleBase> car_;                                      // 主车对象，仅用于初始化
+        car_param car_param_;                                                   // 主车参数
+    };
+} // namespace Planning
+#endif // CAR_MOVE_CMD_H_
