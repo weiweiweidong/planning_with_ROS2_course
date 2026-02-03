d0a8bb1c76f7e46390b9cb77a71b6612133b1a36
weiweiweidong <dongweisp@gmail.com>
2026-02-03 22:18:22 +0900

    完成轨迹的路径部分

--- src/planning/src/local_planner/local_trajectory_combiner.cpp
+++ src/planning/src/local_planner/local_trajectory_combiner.cpp
@@ -2,8 +2,42 @@
 
 namespace Planning
 {
-    LocalTrajectoryCombiner::LocalTrajectoryCombiner()    // 轨迹合成器
+    LocalTrajectoryCombiner::LocalTrajectoryCombiner() // 轨迹合成器
     {
         RCLCPP_INFO(rclcpp::get_logger("local_trajectory"), "local_trajectory_combiner created");
+
+        // 读取配置文件 (因为没有填写配置参数，实际上在这一部分中用不到。为了维护代码风格的统一，就添加了读取的操作)
+        trajectory_config_ = std::make_unique<ConfigReader>();
     }
-}   // namespace Planning
+
+    // 合成轨迹
+    LocalTrajectory LocalTrajectoryCombiner::combin_trajectory(const LocalPath &path, const LocalSpeeds &speeds)
+    {
+        const int path_size = path.local_path.size();
+        const int speeds_size = speeds.local_speeds.size();
+        local_trajectory_.header = path.header;
+        local_trajectory_.local_trajectory.clear();
+
+        if (path_size < 3 || speeds_size < 3) // 少于3个点，就视为空
+        {
+            RCLCPP_WARN(rclcpp::get_logger("trajectory"), "local path or speed empty!");
+            return local_trajectory_;
+        }
+
+        LocalTrajectoryPoint point_tmp;
+        for (int i = 0; i < path_size; i++)
+        {
+            // 路径部分的填充
+            point_tmp.path_point = path.local_path[i]; // 真实路径第i个点的数据，全部打包拷贝到临时点里面的路径点
+            local_trajectory_.local_trajectory.emplace_back(point_tmp);
+
+            // 速度部分的填充
+
+            // 填充进轨迹
+        }
+
+        RCLCPP_INFO(rclcpp::get_logger("tragectory"), "trajectory combined, size = %ld", local_trajectory_.local_trajectory.size());
+        return local_trajectory_;
+    }
+
+} // namespace Planning
--- src/planning/src/local_planner/local_trajectory_combiner.h
+++ src/planning/src/local_planner/local_trajectory_combiner.h
@@ -2,16 +2,29 @@
 #define LOCAL_TRAJECTORY_COMBINER_H_
 
 #include "rclcpp/rclcpp.hpp"
+#include "base_msgs/msg/local_path.hpp"
+#include "base_msgs/msg/local_speeds.hpp"
+#include "base_msgs/msg/local_trajectory.hpp"
+#include "base_msgs/msg/local_trajectory_point.hpp"
 #include "config_reader.h"
 
 namespace Planning
 {
-    class LocalTrajectoryCombiner  // 轨迹合成器
-    {
-        public:
-            LocalTrajectoryCombiner();
+    using base_msgs::msg::LocalPath;
+    using base_msgs::msg::LocalSpeeds;
+    using base_msgs::msg::LocalTrajectory;
+    using base_msgs::msg::LocalTrajectoryPoint;
 
-        private:
+    class LocalTrajectoryCombiner // 轨迹合成器
+    {
+    public:
+        LocalTrajectoryCombiner();
+        LocalTrajectory combin_trajectory(const LocalPath &path, const LocalSpeeds &speeds); // 合成轨迹
+        inline LocalTrajectory local_trajectory() const { return local_trajectory_; }        // 轨迹
+
+    private:
+        std::unique_ptr<ConfigReader> trajectory_config_; // 配置
+        LocalTrajectory local_trajectory_;                // 轨迹
     };
-}  // namespace Planning
-#endif  // LOCAL_TRAJECTORY_COMBINER_H_
+} // namespace Planning
+#endif // LOCAL_TRAJECTORY_COMBINER_H_
