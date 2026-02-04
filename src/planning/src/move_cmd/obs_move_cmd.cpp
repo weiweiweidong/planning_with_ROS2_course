297e1b430005ea7d1f9388326de7e74d22f3b4c2
weiweiweidong <dongweisp@gmail.com>
2026-02-04 23:18:31 +0900

    障碍物车辆的运动

--- src/planning/src/move_cmd/obs_move_cmd.cpp
+++ src/planning/src/move_cmd/obs_move_cmd.cpp
@@ -2,14 +2,71 @@
 
 namespace Planning
 {
-    ObsMoveCmd::ObsMoveCmd() : Node("obs_move_cmd_node")    // 主车运动指令
+    ObsMoveCmd::ObsMoveCmd() : Node("obs_move_cmd_node") // 主车运动指令
     {
-        RCLCPP_INFO(this->get_logger() , "obs_move_cmd_node created");
+        RCLCPP_INFO(this->get_logger(), "obs_move_cmd_node created");
+
+        // 读取配置文件，获取父子坐标
+        obs_move_cmd_config_ = std::make_unique<ConfigReader>();
+        obs_move_cmd_config_->read_move_cmd_config();
+
+        // 初始化：遍历所有的障碍物
+        for (int i = 0; i < 3; i++)
+        {
+            ObsParam obs_param;
+            obs_param.obs_ = std::make_shared<ObsCar>(i + 1); // 因为0是主车，所以障碍物从1开始起算
+            obs_param.obs_broadcaster_ = std::make_shared<TransformBroadcaster>(this);
+
+            obs_param.pos_x_ = obs_param.obs_->loc_point().pose.position.x;
+            obs_param.pos_y_ = obs_param.obs_->loc_point().pose.position.y;
+            obs_param.theta_ = obs_param.obs_->theta();
+            obs_param.speed_ = obs_param.obs_->speed();
+
+            obses_param.emplace_back(obs_param);
+        }
+
+        // 让车辆运动起来
+        timer_ = this->create_wall_timer(
+            0.1s,
+            std::bind(&ObsMoveCmd::obs_broadcast_tf, this));
     }
-}  // namespace Planning
 
+    // 广播坐标变换
+    void ObsMoveCmd::obs_broadcast_tf()
+    {
+        for (auto &obs_param : obses_param)
+        {
+            // 创建消息对象
+            TransformStamped transform_data;
+            transform_data.header.stamp = this->now();
+            transform_data.header.frame_id = obs_move_cmd_config_->pnc_map().frame_;
+            transform_data.child_frame_id = obs_param.obs_->child_frame();
 
-int main(int argc, char *argv[]){
+            // 赋值
+            transform_data.transform.translation.x = obs_param.pos_x_;
+            transform_data.transform.translation.y = obs_param.pos_y_;
+            const double speed_x = obs_param.speed_ * std::cos(obs_param.theta_);
+            const double speed_y = obs_param.speed_ * std::sin(obs_param.theta_);
+            obs_param.pos_x_ += speed_x;
+            obs_param.pos_y_ += speed_y;
+
+            tf2::Quaternion qtn;
+            qtn.setRPY(0.0, 0.0, obs_param.theta_);
+            transform_data.transform.rotation.x = qtn.getX();
+            transform_data.transform.rotation.y = qtn.getY();
+            transform_data.transform.rotation.x = qtn.getZ();
+            transform_data.transform.rotation.w = qtn.getW();
+
+            // 广播消息
+            RCLCPP_INFO(this->get_logger(), "obs_move_cmd broadcast, id: %d, pos: (%.2f , %.2f), speed_xy: (%.2f, %.2f), theta: %.2f",
+                        obs_param.obs_->id(), obs_param.pos_x_, obs_param.pos_y_, speed_x, speed_y, obs_param.theta_);
+            obs_param.obs_broadcaster_->sendTransform(transform_data);
+        }
+    }
+} // namespace Planning
+
+int main(int argc, char *argv[])
+{
     rclcpp::init(argc, argv);
     auto node = std::make_shared<Planning::ObsMoveCmd>();
     rclcpp::spin(node);
--- src/planning/src/move_cmd/obs_move_cmd.h
+++ src/planning/src/move_cmd/obs_move_cmd.h
@@ -2,16 +2,41 @@
 #define OBS_MOVE_CMD_H_
 
 #include "rclcpp/rclcpp.hpp"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "tf2/LinearMath/Quaternion.h"
+#include <cmath>
 #include "config_reader.h"
 #include "obs_car_info.h"
 
 namespace Planning
 {
-    class ObsMoveCmd : public rclcpp::Node  // 主车运动指令
+    using namespace std::chrono_literals;
+    using geometry_msgs::msg::TransformStamped;
+    using tf2_ros::TransformBroadcaster;
+
+    struct ObsParam // 障碍物参数
     {
-        public:
-            ObsMoveCmd();
-        private:
+        std::shared_ptr<VehicleBase> obs_;                      // 障碍物
+        std::shared_ptr<TransformBroadcaster> obs_broadcaster_; // 坐标广播器
+        double pos_x_ = 0.0;
+        double pos_y_ = 0.0;
+        double theta_ = 0.0;
+        double speed_ = 0.0;
     };
-}  // namespace Planning
-#endif  // OBS_MOVE_CMD_H_
+
+    class ObsMoveCmd : public rclcpp::Node // 主车运动指令
+    {
+    public:
+        ObsMoveCmd();
+
+    private:
+        void obs_broadcast_tf(); // 广播障碍物坐标变换
+
+    private:
+        std::unique_ptr<ConfigReader> obs_move_cmd_config_; // 配置
+        std::vector<ObsParam> obses_param;                  // 障碍物参数合集
+        rclcpp::TimerBase::SharedPtr timer_;                // 定时器
+    };
+} // namespace Planning
+#endif // OBS_MOVE_CMD_H_
